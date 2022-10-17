ba9a3b904cc39dbec930cacb02c1ae5fedd293e2
allexandrra <alinaalexandragazdaru@gmail.com>
2022-10-17 23:19:51 +0300

    updated router class and moved header files in include folder

--- include/Router.h
+++ include/Router.h
@@ -1,33 +1,53 @@
 #ifndef ROUTER_H
 #define ROUTER_H
 
-#include <string.h>
-#include <stdint.h>
-#include <vector.h>
-#include "Interface.h"
-#include "Peer.h"
+#include <fstream>
+#include <iostream>
+#include <sstream> 
+#include <iomanip>
+#include <cstring>
+#include <vector>
 
-using namespace Interface;
-using namespace Peer;
+//#include "Interface.h"
+//#include "Peer.h"
 
-namespace Router {
-    class Router {
-        public:
-            Router(int router_ID, int AS, vector<Interface::Interface> interfaces(), vector<int> neighbours(), vector<Peer::Peer> routing_table());
+struct Peer {
+    std::string network;
+    std::string next_hop;
+    std::string path;
+    int weight;
+    int loc_pref;
+    int AS_path_len;
+    char origin_code;
+    int MED;
+    bool e_vs_i;
+};
 
-            int get_router_ID();
-            int get_router_AD();
-            vector<Interface::Interface> get_router_int();
-            vector<int> get_router_neigh();
-            vector<Peer::Peer> get_router_rt();
+struct Interface {
+    string name;
+    string ip;
+    string mask;
+    bool status;
+};
 
-        private:
-            int router_ID;
-            int AS;
-            vector<Interface::Interface> interfaces();
-            vector<int> neighbours();
-            vector<Peer::Peer> routing_table();
-    };
-}
+using namespace std;
+
+class Router {
+    public:
+        Router(int router_ID, int AS, std::vector<Interface> interfaces, std::vector<int> neighbours, std::vector<Peer> routing_table);
+
+        int get_router_ID();
+        int get_router_AS();
+        std::vector<Interface> get_router_int();
+        std::vector<int> get_router_neigh();
+        std::vector<Peer> get_router_rt();
+
+    private:
+        int router_ID;
+        int AS;
+        std::vector<Interface> interfaces;
+        std::vector<int> neighbours;
+        std::vector<Peer> routing_table;
+};
 
 #endif
