8d22334266c33561af62f765087e59caee369739
gquetel <quetel.gregor@gmail.com>
2022-11-16 22:14:18 +0200

    Configuration parser JSON

--- README.md
+++ README.md
@@ -1,22 +1,6 @@
 # PPS_project
 
 
-## BNF Grammar of configuration file
-
-```bnf
-"network" ::=  <setter>  "(" <list-AS> ");" 
-<list-AS> ::= ( <AS>  [*("," <AS>)] ) | <empty> 
-<AS> ::= "{" <AS-name> [<AS-links>] <AS-ip> <AS-netmask>  "};"
-<AS-name> ::= "name" <setter> <name> ";"
-<AS-links> ::= "links" <setter> "[" <name> [*("," <name>)] "]" ";"
-<AS-ip> ::= "ip" <setter> <name> 
-<AS-netmask> ::= "netmask" <setter> <name> 
-<setter> ::= (":" | "=" )
-<name>	::= [A-Za-z\*][-A-Za-z0-9_\*]*
-<integer>	::= [-+]?[0-9]+
-<empty> ::= 
-```
-
 ## New format Message : TRUSTRATE
 
 TRUST: 4 bytes
--- src/configuration_parser.cc
+++ src/configuration_parser.cc
@@ -1,79 +1,72 @@
 #include "../include/configuration_parser.h"
 
+#include "../include/json.hpp"
+
+#include <fstream>
 #include <iostream>
 
-#include "../include/libconfig.h++"
-
-using namespace libconfig;
+using json = nlohmann::json;
 using namespace std;
 
-std::vector<AS> load_configuration() {
-  Config cfg;
-  std::vector<AS> routers;
+std::vector<AS>
+load_configuration()
+{
+    std::vector<AS> routers;
+    std::ifstream f(string(filesystem::current_path()) +
+                    "/scratch/PPS_project/configuration_file.json");
+    try
+    {
+        json config_raw = json::parse(f);
 
-  /* Reading the whole configuration file and checking for structure errors */
-  try {
-    cfg.readFile("./configuration_file.cfg");
-  } catch (const FileIOException &fioex) {
-    std::cerr << "configuration_parser : I/O error while reading file."
-              << std::endl;
-    exit(EXIT_FAILURE);
-  } catch (const ParseException &pex) {
-    std::cerr << "configuration_parser : Parse error at " << pex.getFile()
-              << ":" << pex.getLine() << " - " << pex.getError() << std::endl;
-    exit(EXIT_FAILURE);
-  }
+        // Iterate over AS
+        for (auto& element : config_raw)
+        {
+            // parsing of router names
+            if (!element.contains("name"))
+            {
+                std::cerr << "configuration_parse : error while retrieving name "
+                          << "of router " << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            string name = string(element["name"]);
 
-  /* Extracting data from cf */
-  const Setting &root = cfg.getRoot();
-  try {
-    const Setting &as_list = root["network"];
-    int as_count = as_list.getLength();
+            // parsing of router address
+            if (!element.contains("ip"))
+            {
+                std::cerr << "configuration_parse : error while retrieving ip address "
+                          << "of router " << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            string ip = string(element["ip"]);
 
-    for (int i = 0; i < as_count; i++) {
-      const Setting &as_item = as_list[i];
-      string name;
-      string ip;
-      string netmask;
-      vector<string> links;
+            // parsing of router names
+            if (!element.contains("netmask"))
+            {
+                std::cerr << "configuration_parse : error while retrieving netmask "
+                          << "of router " << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            string netmask = string(element["netmask"]);
 
-      // parsing of router names
-      if (!as_item.lookupValue("name", name)) {
-        std::cerr << "configuration_parse : error while retrieving name"
-                  << "of router " << std::endl;
-        exit(EXIT_FAILURE);
-      }
+            AS router = AS(name, ip, netmask);
 
-      // parsing of router ip
-      if (!as_item.lookupValue("ip", ip)) {
-        std::cerr << "configuration_parse : error while retrieving ip"
-                  << "of router " << name << std::endl;
-        exit(EXIT_FAILURE);
-      }
-
-      // parsing of router netmask
-      if (!as_item.lookupValue("netmask", netmask)) {
-        std::cerr << "configuration_parse : error while retrieving netmask "
-                  << "of router " << name << std::endl;
-        exit(EXIT_FAILURE);
-      }
-      AS router = AS(name, ip, netmask);
-
-      // Parsing of router links
-      const Setting &link_settings = as_item.lookup("links");
-      for (int j = 0; j < link_settings.getLength(); j++) {
-        links.push_back(link_settings[j]);
-      }
-      router.set_links(links);
-
-      routers.push_back(router);
-      cout << "> Autonomous system  \"" << name << "\" created" << endl;
+            // Now we add the links
+            if (element.contains("links"))
+            {
+                for (auto& e_link : element["links"])
+                {
+                    router.add_link(e_link);
+                }
+            }
+            routers.push_back(router);
+            cout << "Router  " << name << " created, with IP: " << ip
+                 << ", and netmask: " << netmask << ".\n";
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
     }
 
-  } catch (const SettingNotFoundException &nfex) {
-    std::cerr << "configuration_parser : couldn't retrieve list of AS in"
-              << "configuration file." << std::endl;
-  }
-
-  return routers;
+    return routers;
 }
